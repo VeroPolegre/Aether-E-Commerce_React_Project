62668faef75effdd54861c106c039b15c7236c7f
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-24 14:02:20 +0100

    main pages and components structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-
-function App() {}
+import Header from "./pages/Header/Header";
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes></Routes>
+      </Router>
+    </>
+  );
+}
 
 export default App;
