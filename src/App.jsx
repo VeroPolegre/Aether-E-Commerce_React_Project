b0014ec1fe515b494f8bc4affadbe5b39b793e62
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-27 00:55:40 +0100

    tailwind implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./pages/Header/Header";
+import Home from "./pages/Home/Home";
+import Footer from "./pages/Footer/Footer";
 function App() {
   return (
-    <>
+    <div className="bg-[#1b2838]">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Router>
         <Header />
-        <Routes></Routes>
+        {/*Categories */}
+        {/*Recommended */}
+        {/*SpecialOffers */}
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+        {/*Browse*/}
+        <Footer />
       </Router>
-    </>
+      <div className="h-screen"></div>
+    </div>
   );
 }
 
--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./Header.scss";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="Header">
+      <h1>Header</h1>
+      <Navbar />
+    </header>
+  );
 };
 
 export default Header;
